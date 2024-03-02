f75e61a40250578275f8430f56c1bb754ff12aa7
Rishabh Jain <rishabh3123jain@gmail.com>
2024-03-02 12:11:06 +0530

    soql using rest api

--- force-app/main/default/lwc/changeDataCapture/changeDataCapture.js
+++ force-app/main/default/lwc/changeDataCapture/changeDataCapture.js
@@ -1,4 +1,8 @@
 import { LightningElement } from 'lwc';
-
+import callRestQuery from '@salesforce/apex/LambdaApex.callRestQuery';
 export default class ChangeDataCapture extends LightningElement {
+    async callQuery() {
+        let data = await callRestQuery();
+        console.log(JSON.parse(data))
+    }
 }
