609d331fad7f8a6588add0f850f33dcb2b661e21
Rishabh Jain <rishabh3123jain@gmail.com>
2024-03-02 14:46:56 +0530

    CDC custom channel

--- force-app/main/default/lwc/changeDataCapture/changeDataCapture.js
+++ force-app/main/default/lwc/changeDataCapture/changeDataCapture.js
@@ -1,8 +1,28 @@
 import { LightningElement } from 'lwc';
+import createCustomChannel from '@salesforce/apex/CustomChangeDataChannel.createCustomChannel';
+import createChannelMembers from '@salesforce/apex/CustomChangeDataChannel.createChannelMembers';
+import createEnrichField from '@salesforce/apex/CustomChangeDataChannel.createEnrichField';
 import callRestQuery from '@salesforce/apex/LambdaApex.callRestQuery';
+import createFilterChannel from '@salesforce/apex/CustomChangeDataChannel.createFilterChannel';
 export default class ChangeDataCapture extends LightningElement {
     async callQuery() {
         let data = await callRestQuery();
-        console.log(JSON.parse(data))
+        console.log(data)
+    }
+    async createFilter() {
+        let data = await createFilterChannel();
+        console.log(data)
+    }
+    async createChannel() {
+        let data = await createCustomChannel();
+        console.log(data)
+    }
+    async createChannelMember() {
+        let data = await createChannelMembers();
+        console.log(data)
+    }
+    async createEnrichFields() {
+        let data = await createEnrichField();
+        console.log(data)
     }
 }
